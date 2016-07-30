a7562d53876c78fcbb158684119cb685a093bce8
Josh London <joshua.a.london@gmail.com>
2016-07-30 15:14:00 -0500

    Add basic modal and security statement

--- src/assets/js/word-tally.js
+++ src/assets/js/word-tally.js
@@ -105,4 +105,39 @@ $(document).ready(function() {
 		generateCSV();
 	});
 
+
+
+
+
+
+
+
+
+
+
+	$('.button--security').on('click', function() {
+		$('body').addClass('modal-active');
+		$('.modal.security-statement').addClass('is-active');
+	});
+
+	$('.button--close-modal').on('click', function() {
+		$('body').removeClass('modal-active');
+		$('.modal').removeClass('is-active');
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
